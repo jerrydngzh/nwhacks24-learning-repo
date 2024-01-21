c5614da5b4f9c81c5f2b878249762b67573f00ff
olivia <yli514@my.bcit.ca>
2024-01-20 18:37:32 -0800

    rendered map

--- Project/App.tsx
+++ Project/App.tsx
@@ -5,9 +5,9 @@ import { MiMapView } from '@mappedin/react-native-sdk';
 // See Trial API key Terms and Conditions
 // https://developer.mappedin.com/guides/api-keys
 const options = {
-  clientId: '65a0422df128bbf7c7072349',
-  clientSecret: '5f72653eba818842c16c4fdb9c874ae02100ffced413f638b7bd9c65fd5b92a4',
-  venue: 'life-science-institute'
+  mapId: "657cc670040fcba69696e69e",
+  key: "65a0422df128bbf7c7072349",
+  secret: "5f72653eba818842c16c4fdb9c874ae02100ffced413f638b7bd9c65fd5b92a4",
   // perspective: 'Website',
 };
 
