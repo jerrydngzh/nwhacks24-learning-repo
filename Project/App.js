e12c6256a2d4881dda290031a52cecdfa287b688
unknown <bryandang10@gmail.com>
2024-01-20 16:02:24 -0800

    add tts from expo speech

--- Project/App.js
+++ Project/App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
+import { useState } from 'react';
 
 export default function App() {
+  const [inputText, setInputText] = useState();
+
+  const speak = () => {
+    if (inputText.trim() !== '') {
+      Speech.speak(inputText);
+    } else {
+      Speech.speak("Please enter some text to read.");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter text:</Text>
+        <TextInput 
+          placeholder='Type here...' 
+          onChangeText={(text)=>setInputText(text)} 
+          value={inputText} 
+        />
+        <Button title="Press" onPress={speak} />
       <StatusBar style="auto" />
     </View>
   );
