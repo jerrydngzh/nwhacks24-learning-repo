d826afeb8fe6cb95c1ae72e765a3135f1f7b787a
Johnson Luong <jkl53@sfu.ca>
2024-01-21 01:02:40 -0800

    integrate basic autho lez go

--- mappedin-react-app/src/App.tsx
+++ mappedin-react-app/src/App.tsx
@@ -3,7 +3,9 @@ import "@mappedin/mappedin-js/lib/mappedin.css";
 import { useMemo } from "react";
 import "./App.css";
 import useMapView from "./useMapView";
-import {useVenue} from "./useVenue";
+import { useVenue } from "./useVenue";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
 
 export default function App() {
   // See Trial API key Terms and Conditions
@@ -12,13 +14,22 @@ export default function App() {
     () => ({
       venue: "mappedin-demo-mall",
       clientId: "5eab30aa91b055001a68e996",
-      clientSecret: "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1"
+      clientSecret: "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1",
     }),
     []
   );
 
   const venue = useVenue(options);
-  const { elementRef, mapView, mapRef} = useMapView(venue);
+  const { elementRef, mapView, mapRef } = useMapView(venue);
 
-  return <div id="app" ref={elementRef} />;
+  return (
+    <>
+      <main>
+        <h1>Auth0 Login</h1>
+        <LoginButton/>
+        <LogoutButton/>
+      </main>
+      <div id="app" ref={elementRef} />
+    </>
+  );
 }
